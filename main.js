7a3158d932a2391f9aea36739e0b0f39ab0ee66e
Jose Muñoz <miguejose230@gmail.com>
2022-12-08 21:11:21 -0500

    terminado de la navbar con responsive

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuBurguer = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
-const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
-burguerMenu.addEventListener('click', toggleMobileMenu)
+menuBurguer.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+    mobileMenu.classList.add('inactive')
+    aside.classList.toggle('inactive');
+}
+
